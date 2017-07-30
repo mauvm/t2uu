0b52372299347d13827f31fd8923185bd3693747
Maurits van Mastrigt <maurits@nerdieworks.nl>
2017-07-30 02:09:12 +0200

    Minor improvements.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import { CronJob } from 'cron'
 import T2 from './T2'
 import TPLINK from './TPLINK'
 import config from './config'
+import log from './log'
 
-const t2uu = new T2(config.t2)
+const t2 = new T2(config.t2)
 const tplink = new TPLINK(config.tplink)
 
 async function check () {
-	const { used, total } = await t2uu.usage()
+	log.debug({ type: 'status' }, 'checking')
+	const { used, total } = await t2.usage()
 	if (used > total - config.bufferMB) await tplink.requestExtraGB()
 }
 
 new CronJob('17 */10 * * * *', check, null, true, 'Europe/Amsterdam')
+log.info({ type: 'status' }, 'started')
